12055fdcd155cc16f6ed3d8e0004ef592532d73c
Strive <rufarotapera@hotmail.com>
2025-07-29 13:25:36 +0200

    uptt

--- api/index.py
+++ api/index.py
@@ -930,6 +930,3 @@ def whatsapp_webhook():
                                     list_id = message['interactive']['list_reply']['id']
                                     handle_bot_logic(user_id, list_id)
         return 'OK', 200
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
